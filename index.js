df73ce3e7bf2b7e5d5ac540849787c1e8dc6ba79
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2024-06-14 23:49:15 +0600

    Fixed the header

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require("path");
-const fs = require('fs/promises')
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'))
@@ -13,6 +11,10 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile(path.resolve('./views/about.html'))
+})
+
+app.get('/grinders', (req, res) => {
+    res.sendFile(path.resolve('./views/grinders.html'))
 })
 
 app.get('/beans', (req, res) => {
@@ -27,27 +29,5 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.resolve('./views/contact.html'))
 })
 
-app.post("/registerToNewsletter", async (req, res) => {
-    if (!req.body.email) {
-        res.status(400).send({ error: "The request body must contain an email field." })
-        return;
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(req.body.email)) {
-        res.status(400).send({ error: "Email format is not valid." });
-        return
-    }
-    
-    const data = JSON.parse(await fs.readFile('./data/newsletter.json' , 'utf-8'));
-    
-    if (data.includes(req.body.email)) {
-        res.status(400).send({ error: "Email already exists." });
-        return;
-    } else {
-        data.push(req.body.email)
-        await fs.writeFile('./data/newsletter.json', JSON.stringify(data), 'utf8');
-        res.status(200).send({ data });
-        return;
-    }
-    
-})
 
-app.listen(3000, () => console.log('Running on http://localhost:3000/'))
+app.listen(3000, () => console.log('Running on https://localhost:3000/'))
--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,11 +1,26 @@
-// overlay
+// shadow on scroll
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll > 10) {
+    document.querySelector("header").classList.add("shadow");
+  } else {
+    document.querySelector("header").classList.remove("shadow");
+  }
+});
 
+
+// overlay
 document.querySelector(".overlay-machines").style.display = "none";
 document.querySelector(".overlay-accessories").style.display = "none";
 
-console.log(document.querySelector('.overlay-accessories').style.display)
+console.log(document.querySelector(".overlay-accessories").style.display);
 
 function showOverlay(name) {
+  if (name === "machines")
+    document.querySelector(".overlay-accessories").style.display = "none";
+  if (name === "accessories")
+    document.querySelector(".overlay-machines").style.display = "none";
+
   const overlay = document.querySelector(`.overlay-${name}`);
 
   console.log(overlay.style.display);
